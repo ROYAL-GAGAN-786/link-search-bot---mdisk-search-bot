f6fea42cb61b7f6468b46c8c50e84a887f3aa239
ROYAL-GAGAN-786 <133026312+ROYAL-GAGAN-786@users.noreply.github.com>
2023-06-06 14:07:57 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Greymattersbot/link-search-bot---mdisk-search-bot /Mdisk-Search-Bot
-cd /Mdisk-Search-Bot
+git clone https://github.com/ROYAL-GAGAN-786/link-search-bot---mdisk-search-bot /Mdisk-Search-Bot
+cd /link-search-bot---mdisk-search-bot
 pip3 install -r requirements.txt
-echo "Starting Bot....GreyMatter_Bots"
+echo "Starting Bot.... ROYALGAGAN"
 python3 main.py
